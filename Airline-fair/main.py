275f17c0d824bbbf2c53ef2c7eb074a1917b7539
Muhammad Junaid <70976999+Johnny463@users.noreply.github.com>
2023-07-26 16:50:23 +0500

    Update main.py

--- Airline-fair/main.py
+++ Airline-fair/main.py
@@ -1,118 +1,152 @@
-import pandas as pd
-import numpy as np
-
-
-#read data 
-df=pd.read_excel("Data_Train.xlsx")
-#check null if any
-null=df.isnull().sum()
-
-#to check row of null
-info=df.info()
-#Nan was in route
-route=df[df.Route.isnull()]
-#2 Nan was present in single route row so dropped row
-df.dropna(inplace=True)
-#check null
-nullcheck=df.isnull().sum()
-info=df.info()
-
-#Airline Unique features and counts
-airline=df.Airline.unique()
-airline_counts=df.Airline.value_counts()
-
-#Counts of source and Destination
-source_counts=df.Source.value_counts()
-destination_count=df.Destination.value_counts()
-#dropping Airline Source and Destination
-df=pd.get_dummies(df,drop_first=True, columns=["Airline","Source","Destination"])
-#replacing total stops feature into integer
-stops=df.Total_Stops.unique()
-df.replace({'non-stop':0, '2 stops':2, '1 stop':1, '3 stops':3, '4 stops':4},inplace=True)
-stops=df.Total_Stops.unique()
-
-#Checked Additional infoo having 79 percent no info so dropped the column
-Additional_infoo=df.Additional_Info.value_counts()
-df.drop(columns="Additional_Info",axis=1,inplace=True)
-
-#Resettong index
-df.reset_index(inplace=True)
-#Route was independent so dropped it
-df.drop("Route",axis=1, inplace=True)
-#extracring Month and day into integer then dropped
-df["Journey_Day"]=pd.to_datetime(df["Date_of_Journey"],).dt.day
-df["Journey_Month"]=pd.to_datetime(df["Date_of_Journey"]).dt.month
-df.drop("Date_of_Journey",axis=1,inplace=True)
-
-#extracing duration into hours and minutes then dropping
-duration_lst=[]
-for i in df["Duration"]:
-    duration_lst.append(i)
-tot_duration_hour=[]
-tot_duration_min=[]
-for i in duration_lst:
-    if len(i.split())!=2:
-        if "h" in i:
-            h=int((i).split("h")[0])
-            tot_duration_hour.append(h*60)
-            tot_duration_min.append(0)
-        else:
-            m=int((i).split("m")[0].split("h")[-1])
-            tot_duration_min.append(m)
-            tot_duration_hour.append(0)
-    else:
-        h=int((i).split("h")[0])
-        m=int((i).split("m")[0].split("h")[-1])
-        tot_duration_hour.append(h*60)
-        tot_duration_min.append(m)
-    Duration_in_minutes=[]
-for (i,j) in zip(tot_duration_hour,tot_duration_min):
-    Duration_in_minutes.append(i+j)
-df["Duration_in_minutes"]=Duration_in_minutes    
-df.drop("Duration",axis=1,inplace=True)
-
-#Extracting arrival and destination hours
-df["Dep_Time_hour"]=pd.to_datetime(df["Dep_Time"]).dt.hour
-df.drop("Dep_Time",axis=1,inplace=True)
-df["Arrival_Time_in_Hour"]=pd.to_datetime(df["Arrival_Time"]).dt.hour    
-df.drop(["index","Arrival_Time"],axis=1,inplace=True)
-
-#Saving cleaned data
-df.to_excel("Exploratory Data Analysis of Data_Train.xlsx")
-
-#Splitting train and test data
-x=df.drop("Price",axis=1)
-y=df["Price"]
-
-#Checking important features
-from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test=train_test_split(x,y, random_state=42,test_size=0.3)
-#from sklearn.ensemble import ExtraTreesRegressor
-
-
-# feature_imp=ExtraTreesRegressor().fit(x,y)
-# feature_imp.feature_importances_
-# feature_imp_series=pd.Series(feature_imp.feature_importances_,index=x.columns)
-
-#plot
-# import matplotlib.pyplot as plt
-# plt.figure(figsize=(12,6))
-# feature_imp_series.sort_values().nlargest(26).plot(kind="barh")
-
-#Training
-from sklearn.ensemble import RandomForestRegressor
-rf=RandomForestRegressor().fit(x_train,y_train)
-pred=rf.predict(x_test)
-
-#Performance and fluctuation(errors)
-score=rf.score(x_train,y_train)
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import mean_absolute_error
-mean_squared_error(y_test,pred)
-
-np.sqrt(mean_squared_error(y_test,pred))
-fluctuation=mean_absolute_error(y_test,pred)
-
-#Saving training model into pickel
+from flask import Flask,request,render_template,jsonify
 import pickle
-pickle.dump( rf, open( "Random Forest Airlines Fare Predictions.pkl", "wb" ) )
+import pandas as pd
+
+
+
+app= Flask(__name__)
+
+
+#Model Loading
+model = pickle.load(open('Random Forest Airlines Fare Predictions.pkl', 'rb'))
+
+#App Routing
+@app.route('/',methods=["GET"])
+
+#Rendring  Index
+def Home():
+
+    return render_template('index.html')
+
+#Rendring prediction
+@app.route('/predict',methods=["POST"])
+
+def predict():
+    
+    if request.method == 'POST':
+    
+       ## Airlines=request.form["Airlines"]
+        print(request.is_json)
+        if request.is_json:
+            json_data= request.json
+            selected_airline = json_data["Airlines"] 
+            Total_Stops=json_data["Total_Stops"]
+            Source = json_data["Source"]
+            Destination=json_data["Destination"]  
+            Date_of_Journey=json_data["Date_of_Journey"]
+            Dep_Time=json_data["Dep_Time"]
+            Arrival_Time=json_data["Arrival_Time"]
+            Duration=json_data["Duration"]
+        else:
+            print("hi")
+            selected_airline = request.form["Airlines"] 
+            Total_Stops=request.form["Total_Stops"]
+            Source = request.form["Source"]
+            Destination=request.form["Destination"]  
+            Date_of_Journey=request.form["Date_of_Journey"]
+            Dep_Time=request.form["Dep_Time"]
+            Arrival_Time=request.form["Arrival_Time"]
+            Duration=request.form["Duration"]
+         # Get the value of the "Airlines" field from the form
+         # Assuming this code is part of a web application where form data is being handled
+
+        # Get the value of the "Airlines" field from the form
+      
+        # Set variables based on the selected airline
+        airline_mapping = {
+            'Air_India': 1,
+            'GoAir': 2,
+            'IndiGo': 3,
+            'Jet_Airways': 4,
+            'Jet_Airways_Business': 5,
+            'Multiple_carriers': 6,
+            'Multiple_carriers_Premium_economy': 7,
+            'SpiceJet': 8,
+            'Trujet': 9,
+            'Vistara': 10,
+            'Vistara_Premium_economy': 11
+        }
+        selected_airline = None
+        airline_numeric = airline_mapping.get(selected_airline, 0)
+# Now your selcted airline set to 1 and the rest of the airline variables set to 0.
+# You can use these variables along with other input features for your prediction model.
+
+       #converting categorical value into numeric through mapping
+        stops_mapping = {
+            'non-stop': 0,
+            '1 stop': 1,
+            '2 stops': 2,
+            '3 stops': 3,
+            '4 stops': 4
+            }
+        total_stops = None
+        total_stops_numeric = stops_mapping.get(total_stops, 0)
+       
+
+        # Assuming this code is part of a web application where form data is being handled
+        # Get the value of the "Source" field from the form
+                        
+        #converting categorical value into numeric through mapping               
+        source_mapping = {
+                'Chennai': 1,
+                'Delhi': 2,
+                'Kolkata': 3,
+                'Mumbai': 4 }
+        selected_source= None
+        source_numeric = source_mapping.get(selected_source, 0)
+       
+         #converting categorical value into numeric through mapping       
+        destination_mapping = {
+            'Cochin': 1,
+            'Delhi': 2,
+            'Hyderabad': 3,
+            'Kolkata': 4,
+            'New Delhi': 5
+        }
+        selected_destination = None
+        destination_numeric = destination_mapping.get(selected_destination, 0)
+        
+        categorical_features = [0] * (len(airline_mapping)  + len(source_mapping) + len(destination_mapping))
+        categorical_features[len(airline_mapping)  + len(source_mapping) + destination_numeric - 1] = 1 
+       ##Get Day and Month
+        
+        Date_of_Journey=pd.Series(Date_of_Journey)    
+        Journey_Day=pd.to_datetime(Date_of_Journey).dt.day   
+        Journey_Month=pd.to_datetime(Date_of_Journey).dt.month
+        
+        
+        #into series
+        Dep_Time=pd.Series(Dep_Time)
+        Dep_Time_hour=pd.to_datetime(Dep_Time).dt.hour +12
+ 
+       #get Arrival time and Destination
+       
+        Arrival_Time=pd.Series(Arrival_Time)  
+        Arrival_Time_in_Hour=pd.to_datetime(Arrival_Time).dt.hour           
+        
+        
+        
+        
+        
+        prediction_input = [total_stops_numeric] + categorical_features + [Journey_Day, Journey_Month, int(Duration), Dep_Time_hour, Arrival_Time_in_Hour]
+        
+        #Prediction
+        prediction = model.predict([prediction_input])
+        output=round(prediction[0],2)
+        if request.is_json:
+            
+            return jsonify({"price":output})
+        #output with prediction
+        return render_template('index.html',prediction_text="Your ticket for {} is {}".format(selected_airline,output))
+    else:
+        return render_template('index.html')
+    
+
+        
+if __name__=="__main__":
+    app.run(debug=True)
+
+
+
+
+      
